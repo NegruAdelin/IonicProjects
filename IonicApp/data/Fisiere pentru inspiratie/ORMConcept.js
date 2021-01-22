f59d7d99adea4923f42ee8cba9571dcdbe465b48
NegruAdelin <negru.adelin@gmail.com>
2021-01-22 18:37:51 +0200

    commit

--- IonicApp/data/Fisiere pentru inspiratie/ORMConcept.js
+++
@@ -1,9 +0,0 @@
-const User = require("./models/User");
-
-User.sync({force: true}).then(() => {
-    // Table created
-    return User.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
